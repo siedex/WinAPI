c67a3605bff760daffbf69f9d0a98b08510bcec1
DESKTOP-55HNGRG\siede <siedex@outlook.com>
2017-02-25 23:01:36 +0200

    Code added

--- СП_2/L1/L1/L1.cpp
+++ СП_2/L1/L1/L1.cpp
@@ -2,10 +2,40 @@
 //
 
 #include "stdafx.h"
+#include <windows.h> 
+#include <iostream> 
+using namespace std;
 
+volatile int n;
+
+DWORD WINAPI Add(LPVOID iNum)
+{
+	cout << "Thread is started." << endl;
+	n += (int)iNum;
+	cout << "Thread is finished." << endl;
+	return 0;
+}
 
 int main()
 {
-    return 0;
-}
+	int inc = 10;
+	HANDLE hThread;
+	DWORD IDThread;
 
+	cout << "n = " << n << endl;
+
+
+	hThread = CreateThread(NULL, 0, Add, (void*)inc, 0, &IDThread);
+	if (hThread == NULL)
+		return GetLastError();
+
+	cout << "hThread = " << hThread << endl;
+
+
+	WaitForSingleObject(hThread, INFINITE);
+
+	CloseHandle(hThread);
+
+	cout << "n = " << n << endl;
+	return 0;
+}
